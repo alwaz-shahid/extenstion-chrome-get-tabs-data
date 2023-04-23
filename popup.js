bed5a09b57e46b88382e14ba8aa197416197602c
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-23 15:00:30 +0500

    window, desc, fixes

--- popup.js
+++ popup.js
@@ -1,30 +1,47 @@
 function saveToFile() {
-  chrome.tabs.query({}, function(tabs) {
-    var tabData = "";
+  chrome.tabs.query({}, function (tabs) {
+    var tabData = '';
     for (var i = 0; i < tabs.length; i++) {
-      tabData += tabs[i].title + "\t" + tabs[i].url + "\n";
+      tabData += tabs[i].title + '\t' + tabs[i].url + '\n';
     }
 
-    var blob = new Blob([tabData], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "tabs.txt");
+    var blob = new Blob([tabData], { type: 'text/plain;charset=utf-8' });
+    var url = URL.createObjectURL(blob);
+    chrome.downloads.download({
+      url: url,
+      filename: 'tabs.txt',
+      saveAs: true,
+    });
   });
 }
 
+chrome.tabs.query({}, function (tabs) {
+  var tabList = document.getElementById('tabList');
+
+  for (var i = 0; i < tabs.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = tabs[i].title;
+    tabList.appendChild(li);
+  }
+});
+
 function copyLinks() {
-  chrome.tabs.query({}, function(tabs) {
-    var links = "";
+  chrome.tabs.query({}, function (tabs) {
+    var links = '';
     for (var i = 0; i < tabs.length; i++) {
-      links += tabs[i].url + "\n";
+      links += tabs[i].url + '\n';
     }
 
-    var dummy = document.createElement("textarea");
+    var dummy = document.createElement('textarea');
     document.body.appendChild(dummy);
     dummy.value = links;
     dummy.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
     document.body.removeChild(dummy);
   });
 }
 
-document.getElementById("saveToFile").addEventListener("click", saveToFile);
-document.getElementById("copyLinks").addEventListener("click", copyLinks);
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('saveToFile').addEventListener('click', saveToFile);
+  document.getElementById('copyLinks').addEventListener('click', copyLinks);
+});
